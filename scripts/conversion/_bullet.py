d2010e431ceb7ad48de6b669fb07c538f70857af
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-11-22 18:02:06 +0900

    Add a script.

---
+++ scripts/conversion/_bullet.py
@@ -0,0 +1,17 @@
+"""
+[caption]
+def=&Bullet(Markdown)
+ja=箇条書きにする(Markdown記法)(&B)
+"""
+
+import sys, io, re
+
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
+if not sys.stdin.isatty() or len(sys.argv) == 2:
+  text = sys.argv[-1] if len(sys.argv) == 2 else sys.stdin.read()
+
+  print(re.sub(r"^.*$", "* \g<0>", text, flags=re.M))
+
