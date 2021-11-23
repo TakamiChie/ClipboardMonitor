c4b33f2297da5e192c42f70bf0565e14fc863992
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-11-23 15:12:02 +0900

    Add script test.

---
+++ scripts/tests/test_runscripts1.py
@@ -0,0 +1,42 @@
+import unittest
+import io
+import sys
+from pathlib import Path
+
+class TestRunScripts1(unittest.TestCase):
+
+  def setUp(self) -> None:
+    self.naturestdin = sys.stdin
+    self.naturestdout = sys.stdout
+    sys.stdin = io.StringIO()
+    sys.stdout = io.StringIO()
+    return super().setUp()
+
+  def tearDown(self) -> None:
+    sys.stdin = self.naturestdin
+    sys.stdout = self.naturestdout
+    return super().tearDown()
+
+  def _checkscript(self, inputstr: str, scriptname: str) -> str:
+    sys.stdin.write(f" {inputstr}")
+    sys.stdin.seek(0)
+    script = ""
+    with open(Path(__file__).parent.parent / f"{scriptname}.py", "r", encoding="utf-8") as f:
+      for s in f.readlines():
+        if not s.startswith("sys.std"): script += f"{s}\n";
+    exec(script)
+    return sys.stdout.getvalue()
+
+  def test_charcount_string(self):
+    """
+    If you pass a string with the following conditions to _charcount.py, make sure that only the character count is returned.
+    * String without URL
+    """
+    self.assertRegex(self._checkscript("test", "run/_charcount"), r"4 char\(s\)")
+
+  def test_charcount_url(self):
+    """
+    If you pass a string with the following conditions to _charcount.py, make sure that only the character count is returned.
+    * URL
+    """
+    self.assertRegex(self._checkscript("http://example.com/", "run/_charcount"), r"19 char\(s\)")
