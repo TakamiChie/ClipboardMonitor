d99320ea1102e46f3bb34317903d1c47a8bedaa4
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-11-24 18:12:52 +0900

    Changed the description of the source code to match the other test code.

--- scripts/tests/test_charcount.py
+++ scripts/tests/test_charcount.py
@@ -4,26 +4,28 @@ import subprocess
 
 class TestCharCount(BaseClass):
   
+  FILE = "run/_charcount"
+  
   def test_charcount_blank(self):
     """
     If you pass a string with the following conditions to _charcount.py, Confirm that it is returned as 0 characters.
     * No character string specified.
     """
-    self.assertRegex(self._checkscript("", "run/_charcount"), r"0 char\(s\)")
+    self.assertEqual(self._checkscript("", self.FILE).split("\n")[0], "<p>0 char(s)</p>")
 
   def test_charcount_string(self):
     """
     If you pass a string with the following conditions to _charcount.py, make sure that only the character count is returned.
     * String without URL
     """
-    self.assertRegex(self._checkscript("test", "run/_charcount"), r"4 char\(s\)")
+    self.assertEqual(self._checkscript("test", self.FILE).split("\n")[0], "<p>4 char(s)</p>")
 
   def test_charcount_url(self):
     """
     If you pass a string with the following conditions to _charcount.py, make sure that only the character count is returned.
     * URL
     """
-    self.assertRegex(self._checkscript("http://example.com/", "run/_charcount"), r"19 char\(s\)")
+    self.assertEqual(self._checkscript("http://example.com/", self.FILE).split("\n")[0], "<p>19 char(s)</p>")
 
   def test_charcount_mdwc_not_installed(self):
     """
@@ -35,7 +37,7 @@ class TestCharCount(BaseClass):
     try:
       os.environ["PATH"] = ';'.join(filter(lambda s: not r"\npm" in s, os.environ["PATH"].split(";")))
       try:
-        self.assertNotRegex(self._checkscript("test", "run/_charcount"), r"Markdown")
+        self.assertEqual(self._checkscript("test", self.FILE).split("\n")[1], "<p>Markdown</p>")
       except subprocess.CalledProcessError: # CalledProcessError = mdwc not installed
         pass        
     finally:
