2d841b8ce8054cf4d06b28b7e6afb879dc718b2b
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-11-25 14:33:25 +0900

    Changed so that if there is no text to be passed, nothing will be printed.

--- scripts/conversion/_leaveurl.py
+++ scripts/conversion/_leaveurl.py
@@ -13,5 +13,6 @@ sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 if not sys.stdin.isatty() or len(sys.argv) == 2:
   text = sys.argv[-1] if len(sys.argv) == 2 else sys.stdin.read()
 
-  m = re.search(r"(https?)(:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+)", text)
-  print(m[0] if m else text)
+  if text != "":
+    m = re.search(r"(https?)(:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+)", text)
+    print(m[0] if m else text)
