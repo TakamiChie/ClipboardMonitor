3603ecabbd8384b3e46c665cf2b882057bc97be4
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-12-09 14:02:04 +0900

    Created a script to create a release package.

---
+++ releasepkg.py
@@ -0,0 +1,55 @@
+## Search Lazarus Root
+import winreg
+from pathlib import Path
+import xml.etree.ElementTree as ElementTree
+import subprocess
+import zipfile
+
+APPLICATION_NAME = "CMon"
+APPLICATION_EXENAME = f"{APPLICATION_NAME}.exe"
+APPLICTAION_LPIFILE = f"{APPLICATION_NAME}.lpi"
+
+lazarus_root = None
+print(">> Find Lazarus")
+with winreg.OpenKeyEx(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\lazarus_is1',
+  access=winreg.KEY_READ | winreg.KEY_WOW64_64KEY) as k:
+  lazarus_root, _ = winreg.QueryValueEx(k, "InstallLocation")
+
+if lazarus_root is None: raise Exception("Lazarus Not Found!")
+print(f"Found! {lazarus_root}")
+
+## Get Version
+print(">> Get info")
+tree = ElementTree.parse(APPLICTAION_LPIFILE)
+vi = tree.find("ProjectOptions").find("VersionInfo")
+ver = lambda n: 0 if n is None else int(n.get('Value')) 
+vn = f"{ver(vi.find('MajorVersionNr'))}.{ver(vi.find('MinorVersionNr'))}.{ver(vi.find('RevisionNr'))}.{ver(vi.find('BuildNr'))}"
+print((f"Version:{vn}"))
+
+ch = subprocess.check_output("git log -n 1 --pretty=format:%h".split(" ")).decode("UTF-8")
+print((f"CommitHash:{ch}"))
+
+print(">> Create Release Package")
+proc = subprocess.Popen(f"{lazarus_root}\\lazbuild --build-mode=Release {APPLICTAION_LPIFILE}", shell=True);
+proc.communicate()
+
+rootdir = Path(__file__).parent
+fn = rootdir / f"{APPLICATION_NAME}-{vn}-{ch}.zip"
+with zipfile.ZipFile(fn, "w", 
+  compression=zipfile.ZIP_DEFLATED) as zfile:
+  if (rootdir / f"R{APPLICATION_EXENAME}").exists():
+    zfile.write(f"R{APPLICATION_EXENAME}", arcname=f"{APPLICATION_EXENAME}")
+  else:
+    zfile.write(f"{APPLICATION_EXENAME}")
+  for f in rootdir.iterdir():
+    if f.is_file():
+      if f.name in ["LICENSE"] or f.name.endswith(".md"):
+        zfile.write(f.name)
+    if f.is_dir():
+      if f.name.endswith(".assets"):
+        zfile.write(f.name)
+        for d in f.iterdir():
+          if d.is_file():
+            zfile.write(f"{f.name}/{d.name}")
+
+print(f"Release Package Create Complete! at '{fn}")
