f4001d1c4f85dc33b86641bbcecc22164c032d1d
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-12-14 13:12:36 +0900

    Change the script folder.

--- resourcemaker.py
+++ resourcemaker.py
@@ -13,7 +13,8 @@ lazres = Path(args.lazarus_dir) / "tools" / "lazres"
 
 cmds = ["scripts.lrs"]
 
-for f in Path(__file__).parent.glob("**/_*.py"):
+parentdir = Path(__file__).parent
+for f in parentdir.glob("scripts/**/_*.py"):
   if f.parent.name in ["conversion", "run"]:
     cmds.append(f"{f}={f.parent.name[0]}{f.name[1:]}")
 
@@ -24,7 +25,6 @@ print(subprocess.check_output(cmdline).decode("UTF-8"))
 ### Add Version Information
 
 date = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
-parentdir = Path(__file__).parent.parent
 chash = subprocess.check_output("git log -n 1 --pretty=format:commit_hash=%h".split(" "), cwd=str(parentdir)).decode("UTF-8")
 cdate = subprocess.check_output("git log -n 1 --pretty=format:commit_date=%cd --date=format:%Y/%m/%d".split(" "), cwd=str(parentdir)).decode("UTF-8")
 ini = f"""
